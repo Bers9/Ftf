d4a06a17ae084c4d22db4f1fcb7f03b4ff58b69a
Bers9 <maja898967@gmail.com>
2022-11-03 20:21:05 +0300

    part 1 часть 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+a=int(input("Введите число: ")) 
 
+counter=0
+summ=0
+counter_zeros=0
+while a!=0:
+    d=a%10
+    a//=10
+
+    counter +=1
+    summ+=d
+
+    if d==0:
+        counter_zeros +=1
+
+print("Сумма", summ)
+print("Кол-во цифр: ", counter)
+print("Ср.ариф.: ", summ/counter)
+print("Количество нулей", counter_zeros)
